45a78ab3f40d732cd0eb31959902d9d2984d027c
mehul.bafnaa <mehulbafna694@gmail.com>
2024-09-12 17:53:03 -0400

    Added docstrings

--- GMM.py
+++ GMM.py
@@ -2,6 +2,29 @@ import numpy as np
 from scipy.special import logsumexp
 
 class GMM:
+    """
+    Gaussian Mixture Model (GMM) implementation.
+
+    This class implements the GMM algorithm for clustering and density estimation.
+
+    Parameters:
+    -----------
+    n_components : int
+        The number of mixture components.
+    covariance_type : str, optional (default='full')
+        The type of covariance parameters to use. 
+        Must be one of 'full', 'tied', 'diag', 'spherical'.
+    n_iter : int, optional (default=100)
+        The number of EM iterations to perform.
+    tol : float, optional (default=1e-5)
+        The convergence threshold. EM iterations will stop when the lower bound 
+        average gain is below this threshold.
+    reg_covar : float, optional (default=1e-6)
+        Non-negative regularization added to the diagonal of covariance.
+    random_state : int, optional (default=None)
+        Controls the random seed given to the method for initialization.
+    """
+
     def __init__(self, n_components, covariance_type='full', n_iter=100, tol=1e-5, reg_covar=1e-6, random_state=None):
         self.n_components = n_components
         self.covariance_type = covariance_type
@@ -11,6 +34,14 @@ class GMM:
         self.random_state = random_state
 
     def initialize_parameters(self, X):
+        """
+        Initialize the model parameters.
+
+        Parameters:
+        -----------
+        X : array-like, shape (n_samples, n_features)
+            The input data.
+        """
         n_samples, n_features = X.shape
         rng = np.random.RandomState(self.random_state)
 
@@ -27,6 +58,19 @@ class GMM:
             self.covariances = np.ones(self.n_components)
 
     def log_gaussian_pdf(self, X):
+        """
+        Compute the log Gaussian probability density function.
+
+        Parameters:
+        -----------
+        X : array-like, shape (n_samples, n_features)
+            The input data.
+
+        Returns:
+        --------
+        log_prob : array, shape (n_samples, n_components)
+            Log probabilities of each data point in X.
+        """
         n_samples, n_features = X.shape
         log_prob = np.zeros((n_samples, self.n_components))
 
@@ -57,6 +101,19 @@ class GMM:
         return log_prob
 
     def expectation_step(self, X):
+        """
+        E-step: compute the expectation of the latent variables.
+
+        Parameters:
+        -----------
+        X : array-like, shape (n_samples, n_features)
+            The input data.
+
+        Returns:
+        --------
+        resp : array, shape (n_samples, n_components)
+            Posterior probabilities of each mixture component for each observation.
+        """
         log_prob = self.log_gaussian_pdf(X)
         log_weighted_prob = log_prob + np.log(self.weights)
         log_prob_norm = logsumexp(log_weighted_prob, axis=1)
@@ -64,6 +121,16 @@ class GMM:
         return np.exp(log_resp)
 
     def maximization_step(self, X, resp):
+        """
+        M-step: maximize the expected complete-data log-likelihood.
+
+        Parameters:
+        -----------
+        X : array-like, shape (n_samples, n_features)
+            The input data.
+        resp : array-like, shape (n_samples, n_components)
+            Posterior probabilities of each mixture component for each observation.
+        """
         n_samples, n_features = X.shape
         weights = resp.sum(axis=0)
         self.weights = weights / n_samples
@@ -94,6 +161,19 @@ class GMM:
             self.covariances += self.reg_covar
 
     def fit(self, X):
+        """
+        Estimate model parameters with the EM algorithm.
+
+        Parameters:
+        -----------
+        X : array-like, shape (n_samples, n_features)
+            The input data.
+
+        Returns:
+        --------
+        self : object
+            Returns self.
+        """
         self.initialize_parameters(X)
         prev_log_likelihood = -np.inf
 
@@ -108,19 +188,70 @@ class GMM:
         return self
 
     def predict(self, X):
+        """
+        Predict the labels for the data samples in X using trained model.
+
+        Parameters:
+        -----------
+        X : array-like, shape (n_samples, n_features)
+            The input data.
+
+        Returns:
+        --------
+        labels : array, shape (n_samples,)
+            Component labels.
+        """
         resp = self.expectation_step(X)
         return np.argmax(resp, axis=1)
 
     def compute_log_likelihood(self, X):
+        """
+        Compute the log-likelihood of the data under the model.
+
+        Parameters:
+        -----------
+        X : array-like, shape (n_samples, n_features)
+            The input data.
+
+        Returns:
+        --------
+        log_likelihood : float
+            Log-likelihood of the data.
+        """
         log_prob = self.log_gaussian_pdf(X)
         return np.sum(logsumexp(np.log(self.weights) + log_prob, axis=1))
 
     def score_samples(self, X):
+        """
+        Compute the log-likelihood of each sample.
+
+        Parameters:
+        -----------
+        X : array-like, shape (n_samples, n_features)
+            The input data.
+
+        Returns:
+        --------
+        log_prob : array, shape (n_samples,)
+            Log-likelihood of each sample.
+        """
         log_prob = self.log_gaussian_pdf(X)
         return logsumexp(np.log(self.weights) + log_prob, axis=1)
 
     def bic(self, X):
-        """Bayesian Information Criterion"""
+        """
+        Bayesian Information Criterion for the current model on the input X.
+
+        Parameters:
+        -----------
+        X : array-like, shape (n_samples, n_features)
+            The input data.
+
+        Returns:
+        --------
+        bic : float
+            BIC for the current model on the input X.
+        """
         n_samples, n_features = X.shape
         cov_params = {'full': self.n_components * n_features * (n_features + 1) / 2,
                       'tied': n_features * (n_features + 1) / 2,
@@ -130,7 +261,19 @@ class GMM:
         return -2 * self.compute_log_likelihood(X) + n_parameters * np.log(n_samples)
 
     def aic(self, X):
-        """Akaike Information Criterion"""
+        """
+        Akaike Information Criterion for the current model on the input X.
+
+        Parameters:
+        -----------
+        X : array-like, shape (n_samples, n_features)
+            The input data.
+
+        Returns:
+        --------
+        aic : float
+            AIC for the current model on the input X.
+        """
         n_samples, n_features = X.shape
         cov_params = {'full': self.n_components * n_features * (n_features + 1) / 2,
                       'tied': n_features * (n_features + 1) / 2,
